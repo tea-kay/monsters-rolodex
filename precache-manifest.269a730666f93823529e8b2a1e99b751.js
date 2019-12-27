81cb49fbac2feab1f66b2a3539ec929852446716
Timothy Kim <tkim1202@gmail.com>
2019-12-27 17:09:11 -0500

    Updates

---
+++ precache-manifest.269a730666f93823529e8b2a1e99b751.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63af430fa5f08b869934d1d913adbfe9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5d2bc0c1420ee223a4cf",
+    "url": "/monsters-rolodex/static/css/main.faced924.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5d2bc0c1420ee223a4cf",
+    "url": "/monsters-rolodex/static/js/main.fe6bebcc.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
